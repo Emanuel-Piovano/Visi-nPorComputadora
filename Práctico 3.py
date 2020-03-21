8f6fe2ada5c2f6e89e35902fb62e5247cd766790
Emanuel <emapiovano@gmail.com>
2020-03-21 20:32:13 -0300

    Práctico 3

---
+++ Práctico 3.py
@@ -0,0 +1,14 @@
+import cv2
+cap = cv2.VideoCapture(0)
+#Imprimimos los FPS
+print('Los FPS que obtiene la cámara es:', cap.get(5))
+
+while(True):
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame', gray)
+    if((cv2.waitKey(1) & 0xFF) == ord('q')):
+        break
+cap.release()
+cv2.destroyAllWindows()
