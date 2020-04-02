b722f5a7c8ca474415c1d22a7c59da9fc0f36ace
Emanuel <emapiovano@gmail.com>
2020-04-02 20:06:06 -0300

    Práctico 2

--- Práctico 2.py
+++ Práctico 2.py
@@ -3,12 +3,13 @@ import cv2
 #Leemos una imagen
 img = cv2.imread('hoja.png', 0)
 #Medimos la longitud de las filas y columnas del array img
-print(len(img))
-print(len(img[1]))
+(height, width) = img.shape[:2]
 #Definimos que todo valor menor a 240 en el array, va a tomar el valor 0, o sea negro. Los valores mayores a 240 van a seguir teniendo su valor original
-for y in range (0,625):
-    for x in range (0,625):
+for y in range(0, height):
+    for x in range(0, width):
         if img[x][y] < 240:
             img[x][y] = 0
+        else:
+            img[x][y] = 255
 #Generamos una nueva imágen a partir del array modificado
 cv2.imwrite('resultado.png', img)
