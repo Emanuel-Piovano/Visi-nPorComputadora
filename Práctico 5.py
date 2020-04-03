ea90c03042b4e2a4be87a2883a67613ccdf1322b
Emanuel <emapiovano@gmail.com>
2020-04-03 20:19:50 -0300

    Modificación
    Se agregó que al seleccionar un rectángulo y al oprimir la letra "g", se guarda lo que se encuentra dentro del rectángulo en una nueva imagen

--- Práctico 5.py
+++ Práctico 5.py
@@ -1,18 +1,16 @@
 import cv2
-import time
 
 img = cv2.imread('La libertad guiando al pueblo.jpg', cv2.IMREAD_COLOR)
-drawing = False # true if mouse is pressed
-ix, iy = -1, -1
+x1, y1, x2, y2 = -1, -1, 1, -1
 
 def draw(event, x, y, flags, param):
-    global ix, iy, drawing, mode
+    global x1, y1, x2, y2
     if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix, iy = x, y
+        x1, y1 = x, y
     elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        cv2.rectangle(img, (ix, iy), (x, y), (255, 255, 255), 5)
+        x2, y2 = x, y
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+        return (x1, x2, y1, y2)
 
 cv2.namedWindow('Imagen')
 cv2.setMouseCallback('Imagen', draw)
@@ -20,7 +18,18 @@ while(True):
     cv2.imshow('Imagen', img)
     tecla = cv2.waitKey(20)
     if tecla == ord('g'):
-        cv2.imwrite('La libertad guiando al pueblo-editado-.jpg', img)
+        print('x1:', x1, 'x2:', x2, 'y1:', y1, 'y2:', y2)
+        if (y2 >= y1) & (x2 >= x1):
+            crop_img = img[y1:y2, x1:x2]
+        elif (y2 >= y1) & (x2 < x1):
+            crop_img = img[y1:y2, x2:x1]
+        elif (y2 < y1) & (x2 >= x1):
+            crop_img = img[y2:y1, x1:x2]
+        else:
+            crop_img = img[y2:y1, x2:x1]
+        cv2.imshow('Imagen recortada', crop_img)
+        cv2.waitKey(0)
+        cv2.imwrite('La libertad guiando al pueblo-editado-.jpg', crop_img)
         break
     elif tecla == ord('r'):
         img = cv2.imread('La libertad guiando al pueblo.jpg', cv2.IMREAD_COLOR)
